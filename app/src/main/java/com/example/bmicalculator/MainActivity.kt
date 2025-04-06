734843bf8c99bb2a29e32c6125fe246a505dcd38
Martin-ing <23002494@galileo.edu>
2025-04-05 18:32:07 -0600

    creando variables y funciones

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bmicalculator.ui.theme.BMICalculatorTheme
 
@@ -19,29 +18,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BMICalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                BMICalc()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BMICalc() {
+    var weight by remember { mutableStateOf("") }
+    var height by remember { mutableStateOf("") }
+    var bmiResult by remember { mutableStateOf<Float?>(null) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        BMICalcForm(
+            weight = weight,
+            height = height,
+            onWeightChange = { weight = it },
+            onHeightChange = { height = it },
+            onCalculateClick = {
+                val newWeight = weight.toFloatOrNull() ?: 0f
+                val newHeight = height.toFloatOrNull() ?: 0f
+                if (newHeight > 0) {
+                    bmiResult = newWeight / (newHeight * newHeight)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun BMICalcForm(weight: String, height: String, onWeightChange: (String) -> Unit, onHeightChange: (String) -> Unit, onCalculateClick: () -> Unit) {
+
+}
+
+@Composable
+fun BMICalcResult() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BMICalculatorTheme {
-        Greeting("Android")
+        BMICalc()
     }
 }
